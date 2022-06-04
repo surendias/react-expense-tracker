4ef36369374bc33a393fb0443ec12e7ce9d8a72d
Suren Dias <suren@surendias.com>
2022-06-05 02:49:29 +0530

    started seperating components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function ExpenseItem() {
-  return <div>ExpenseItem</div>;
+  return (
+    <div className="p-8 m-5 border-4 border-solid rounded border-white-400">
+      <strong>Cargills: LKR 2900.00</strong> <br />
+      <span>28/03/2022 05:00 PM</span>
+    </div>
+  );
 }
 
 export default ExpenseItem;
